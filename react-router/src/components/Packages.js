edcbd92f8d940a501bc7a58db31f61564d2e36d6
wbrewster02 <wbrewster02@gmail.com>
2023-06-04 12:25:37 -0400

    added routes, router, etc

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,12 +1,15 @@
 
 
-export default function Packages() {
-
+export default function Packages({ packages }) {
+    const display = packages.map(p => <li>{p}</li>)
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
+                    <ul>
+                        {display}
+                    </ul>
                 </div>
                 
             </div>
